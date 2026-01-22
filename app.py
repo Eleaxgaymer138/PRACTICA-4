2d43454b9bddd0155c1b5cf73508254c2b033c72
Eleazar Reyes <158332678+Eleaxgaymer138@users.noreply.github.com>
2026-01-21 21:01:00 -0700

    Segundo commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Crea una instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Define una ruta (URL) para la página principal "/"
+@app.route('/')
+def hola_mundo():
+    # Lo que se devuelve aquí se mostrará en el navegador
+    return '¡Hola! Mi pipeline de DevOps funciona correctamente.'
+
+# Verifica si este archivo es el programa principal
+if __name__ == '__main__':
+    # Inicia el servidor web
+    app.run(host='0.0.0.0', port=5000)
